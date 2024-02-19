d00bce0b5e8f741290d44e20a83e7edac2363962
sddemir <sumeyyedemir1903@gmail.com>
2024-02-19 16:39:50 +0300

    new advice everytime, this time with dignity

--- advice-generator-app-main/app.js
+++ advice-generator-app-main/app.js
@@ -13,12 +13,11 @@ console.log('anan');
 // }
 dice.addEventListener('click', async function getAdvice(){
     try {
-        const timestamp = new Date().getTime(); // Get current timestamp
-        const response = await axios.get(`https://api.adviceslip.com/advice?timestamp=${timestamp}`);
-        console.log(response.data); // Log the response data to see what's being returned
+        const slip_id = Math.floor(Math.random() * 200) + 1;
+        const response = await axios.get(`https://api.adviceslip.com/advice/${slip_id}`);
+        console.log(response.data); 
         document.getElementById('advice').innerText = response.data.slip.advice;
     } catch (error) {
         console.error('Error fetching advice:', error);
     }
 });
-
